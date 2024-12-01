2c76ece5ba23edc1457b001d8ad71fdfa4e8aba3
Matthz-max <matheus7serafim@gmail.com>
2024-12-01 11:12:54 -0300

    Add files via upload

---
+++ integrador-front-e-back-main/integrador-front-e-back-main/projeto-integrador-back-main/projeto-integrador-back-main/back/back/src/main/java/com/api/integrador/back/repository/CarroRepository.java
@@ -0,0 +1,13 @@
+package com.api.integrador.back.repository;
+ 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.integrador.back.dto.CarroDTO;
+import com.api.integrador.back.model.Carromodel;
+
+public interface CarroRepository extends JpaRepository<Carromodel, Integer> {
+
+	 
+ 
+}
